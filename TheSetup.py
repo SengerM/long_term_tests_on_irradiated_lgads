d8b43114356c8f66575c62e8ee19d70ec5e36613
Matias Senger <m.senger@hotmail.com>
2021-09-13 12:53:17 +0200

    First tested working version

--- TheSetup.py
+++ TheSetup.py
@@ -47,9 +47,9 @@ class TheSetup:
 		slots_df = slots_df.copy()
 		slots_df.set_index('Slot number', inplace=True)
 		self._caen_outputs_per_slot = {}
-		for slot_number in slots_df['Slot number']:
+		for slot_number in slots_df.index:
 			self._caen_outputs_per_slot[slot_number] = OneCAENChannel(
-				caen = caen_power_supplies[slots_df.loc[slot_number, 'CAEN serial number']],
+				caen = caen_power_supplies[str(slots_df.loc[slot_number, 'CAEN serial number'])],
 				channel_number = slots_df.loc[slot_number, 'CAEN channel number'],
 			)
 	
@@ -65,7 +65,7 @@ class TheSetup:
 		self._climate_chamber.temperature_set_point = celsius
 	
 	@property
-	def temperature_measured(self):
+	def temperature(self):
 		"""Returns the actual value of the temperature in Celsius."""
 		return self._sensirion_sensor.temperature
 	
@@ -78,22 +78,22 @@ class TheSetup:
 	
 	# High voltage methods ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 	
-	def set_slot_bias_voltage(self, slot_number: int, volt: float):
+	def set_bias_voltage(self, slot_number: int, volt: float):
 		"""Set the bias voltage of the specified slot."""
 		_validate_type(slot_number, 'slot_number', int)
 		self._caen_outputs_per_slot[slot_number].V_set = float(volt)
 	
-	def set_slot_current_compliance(self, slot_number: int, ampere: float):
+	def set_current_compliance(self, slot_number: int, ampere: float):
 		"""Set the current compliance for the specified slot."""
 		_validate_type(slot_number, 'slot_number', int)
 		self._caen_outputs_per_slot[slot_number].current_compliance = float(ampere)
 	
-	def measure_slot_bias_voltage(self, slot_number: int):
+	def measure_bias_voltage(self, slot_number: int):
 		"""Returns a measurement of the bias voltage in the specified slot."""
 		_validate_type(slot_number, 'slot_number', int)
 		return self._caen_outputs_per_slot[slot_number].V_mon
 	
-	def measure_slot_bias_current(self, slot_number: int):
+	def measure_bias_current(self, slot_number: int):
 		"""Returns a measurement of the bias current in the specified slot."""
 		_validate_type(slot_number, 'slot_number', int)
 		return self._caen_outputs_per_slot[slot_number].I_mon
@@ -111,5 +111,25 @@ class TheSetup:
 if __name__ == '__main__':
 	import pandas
 	
-	slots_df = pandas.read_excel("/home/alf/cernbox/measurements_data/LGAD/EPR2021_LGAD_long_term_test/daemon/control/slots_definition.ods", engine="odf")
-	check_integrity_of_slots_df(slots_df)
+	slots_df = pandas.read_excel("/home/sengerm/cernbox/measurements_data/LGAD/EPR2021_LGAD_long_term_test/daemon/control/slots_definition.ods", engine="odf")
+	caen_new = CAENDesktopHighVoltagePowerSupply(ip='130.60.165.119', timeout=10)
+	caen_old = CAENDesktopHighVoltagePowerSupply(ip='130.60.165.121', timeout=10)
+	climate_chamber = ClimateChamber(ip = '130.60.165.218', temperature_min = -20, temperature_max = 20)
+	sensirion_sensor = SensirionSensor()
+	setup = TheSetup(
+		climate_chamber = climate_chamber, 
+		sensirion_sensor = sensirion_sensor, 
+		caen_1 = caen_old, 
+		caen_2 = caen_new, 
+		slots_df = slots_df,
+	)
+	
+	print(f'Temperature set point: {setup.temperature_set_point} °C')
+	print(f'Temperature: {setup.temperature} °C')
+	for slot_number in [1,2,3,4,5,6,7,8]:
+		print(f'Voltage in slot {slot_number}: {setup.measure_bias_voltage(slot_number)} V')
+		print(f'Bias current in slot {slot_number}: {setup.measure_bias_current(slot_number)} A')
+	
+	while True:
+		setup.set_bias_voltage(int(input('Slot number to set bias voltag? ')), int(input('Bias voltage? ')))
+		print('Changing bias voltage...')

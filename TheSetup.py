e2751b53ada19edec489e721d8303bc530996d45
Matias Senger <m.senger@hotmail.com>
2021-09-15 17:00:15 +0200

    Implemented `start`

--- TheSetup.py
+++ TheSetup.py
@@ -2,6 +2,7 @@ from threading import RLock
 from VotschTechnikClimateChamber.ClimateChamber import ClimateChamber # https://github.com/SengerM/VotschTechnik-climate-chamber-Python
 from CAENpy.CAENDesktopHighVoltagePowerSupply import CAENDesktopHighVoltagePowerSupply, OneCAENChannel # https://github.com/SengerM/CAENpy
 from SensirionSensor import SensirionSensor
+import time
 
 def check_integrity_of_slots_df(slots_df):
 	"""Checks that there are no errors (like duplicate outputs) in the configuration dataframe for the slots. If there are no errors, this function does nothing. If an error is found, an error is raised. The checks that this function does are very specific of my setup."""
@@ -44,6 +45,7 @@ class TheSetup:
 	
 	MAX_OPERATING_TEMPERATURE = -18 # °C. If the temperature is above this value, it is not possible to set a high voltage.
 	UNBIASED_VOLTAGE_THRESHOLD = 5 # V. Value of bias voltage below which to consider that this is unbiased.
+	NORMAL_OPERATING_TEMPERATURE = -20 # °C.
 	
 	def __init__(self, climate_chamber, sensirion_sensor, caen_1, caen_2, slots_df):
 		"""- slots_df: a Pandas dataframe with columns "Slot name,CAEN model name,CAEN serial number,CAEN channel number"."""
@@ -121,8 +123,11 @@ class TheSetup:
 	def set_bias_voltage(self, slot_name: str, volt: float):
 		"""Set the bias voltage of the specified slot. Before doing so, it checks that the temperature is below the MAX_OPERATING_TEMPERATURE if the voltage is above what is considered as 0 V."""
 		volt = _cast_to_float_number(volt, 'volt')
-		if self.temperature > self.MAX_OPERATING_TEMPERATURE and volt > self.UNBIASED_VOLTAGE_THRESHOLD:
-			raise RuntimeError(f'Trying to set bias voltage for slot {slot_name} to {volt} V while the temperature is {self.temperature} °C, which is > than the maximum operating temperature of {self.MAX_OPERATING_TEMPERATURE} °C.')
+		if volt > self.UNBIASED_VOLTAGE_THRESHOLD:
+			if self.status != 'ready to operate':
+				raise RuntimeError(f'Trying to set bias voltage for slot {slot_name} to {volt} V while the setup status is {repr(self.status)}. It must be in status "ready to operate".')
+			if self.temperature > self.MAX_OPERATING_TEMPERATURE:
+				raise RuntimeError(f'Trying to set bias voltage for slot {slot_name} to {volt} V while the temperature is {self.temperature} °C, which is > than the maximum operating temperature of {self.MAX_OPERATING_TEMPERATURE} °C.')
 		self._get_CAEN_for_(slot_name).V_set = float(volt)
 	
 	def set_current_compliance(self, slot_name: str, ampere: float):
@@ -155,21 +160,58 @@ class TheSetup:
 	
 	@property
 	def status(self):
-		"""Returns the status of the system based on the value of different variables. The status is either "error", "ready to operate", "cooling down" and "warm"."""
+		"""Returns the status of the system based uniquely on the value of different variables, e.g. temperature. See the source code for more info."""
 		ERROR_STATUS = 'error'
-		if self.temperature_set_point < self.MAX_OPERATING_TEMPERATURE:
-			if self.temperature < self.MAX_OPERATING_TEMPERATURE:
-				return 'ready to operate'
-			else: # if temperature > operating temperature
-				if not self._is_any_slot_biased(): # if all unbiased
-					return 'cooling down'
-				else:
+		if self._climate_chamber.is_running == False:
+			return 'not running'
+		else:
+			if self.temperature_set_point < self.MAX_OPERATING_TEMPERATURE:
+				if self.temperature < self.MAX_OPERATING_TEMPERATURE:
+					return 'ready to operate'
+				else: # if temperature > operating temperature
+					if not self._is_any_slot_biased(): # if all unbiased
+						return 'cooling down'
+					else:
+						return ERROR_STATUS
+			else: # if temperature set point > operating temperature
+				if self._is_any_slot_biased():
 					return ERROR_STATUS
-		else: # if temperature set point > operating temperature
-			if self._is_any_slot_biased():
-				return ERROR_STATUS
-			else: # if all unbiased
-				return 'warm'
+				else: # if all unbiased
+					return 'warm'
+	
+	def start(self, humidity_before_cooling_down_percentage=20, humidity_timeout_seconds=60*10, cooling_down_timeout_seconds=60*15):
+		"""Performs the routine to start the setup from "not operating, at room temperature" to "ready to operate at low temperature"."""
+		_validate_type(humidity_before_cooling_down_percentage, 'humidity_before_cooling_down_percentage', int)
+		_validate_type(humidity_timeout_seconds, 'humidity_timeout_seconds', int)
+		_validate_type(cooling_down_timeout_seconds, 'cooling_down_timeout_seconds', int)
+		if self.status != 'not running':
+			raise RuntimeError(f'Before calling the `start` method the setup must be in "not running" status, currently it is in {repr(self.status)} status.')
+		# If we are here it is because everything is fine, this means that all devices are unbiased and the climate chamber is at high temperature.
+		self._climate_chamber.dryer = True
+		self._climate_chamber.compressed_air = True
+		self.temperature_set_point = 20 # We start at room temperature.
+		for slot_name in self.slots_names:
+			self.set_bias_voltage(slot_name, 0) # Just in case we set all bias voltages to 0 so we start fresh.
+		self._climate_chamber.start() # Start the climate chamber. It is at room temperature but dryer is on so it should decrease the humidity.
+		# Wait until humidity decreases ---
+		time_started_drying = time.time()
+		while self.humidity > humidity_before_cooling_down_percentage:
+			time.sleep(11)
+			if time.time()-time_started_drying > humidity_timeout_seconds:
+				raise TimeoutError(f'Cannot reach target humidity of {humidity_before_cooling_down_percentage} %RH after {humidity_timeout_seconds} seconds of operation at room temperature, to proceed with the cooling down. Current humidity is {self.humidity:.2f} %RH.')
+		# If we are here it is because the humidity is low enough to start cooling down ---
+		self.temperature_set_point = self.NORMAL_OPERATING_TEMPERATURE
+		time_started_cooling_down = time.time()
+		time.sleep(1)
+		if self.status != 'cooling down':
+			raise RuntimeError(f'The status of the setup at this point should be "cooling down", instead it is {repr(self.status)}.')
+		while self.status == 'cooling down':
+			time.sleep(11)
+			if time.time()-time_started_cooling_down > cooling_down_timeout_seconds:
+				raise TimeoutError(f'Cannot reach target temperature of {self.NORMAL_OPERATING_TEMPERATURE} °C after {cooling_down_timeout_seconds} seconds. Current temperature is {self.temperature:.2f} °C.')
+		# If we are here, everything should be ready to operate.
+		if self.status != 'ready to operate':
+			raise RuntimeError(f'The status of teh setup at this point should be "ready to operate", instead it is {repr(self.status)}.')
 	
 	# Setup state methods ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 	
@@ -186,6 +228,7 @@ class TheSetup:
 
 if __name__ == '__main__':
 	import pandas
+	import time
 	
 	slots_df = pandas.read_excel("/home/sengerm/cernbox/measurements_data/LGAD/EPR2021_LGAD_long_term_test/daemon/control/slots_definitions.ods", engine="odf")
 	caen_new = CAENDesktopHighVoltagePowerSupply(ip='130.60.165.119', timeout=10)
@@ -201,7 +244,21 @@ if __name__ == '__main__':
 	)
 	
 	print(f'Setup status is "{setup.status}"')
-	setup.temperature_set_point = -20
+	print(f'Climate chamber dryer: {setup.dryer}')
+	print(f'Climate chamber compressed air: {setup.compressed_air}')
+	print(f'Temperature set point: {setup.temperature_set_point} °C')
+	print(f'Temperature: {setup.temperature:.2f} °C')
+	print(f'Humidity: {setup.humidity:.2f} %RH')
+	for idx, slot_name in enumerate(setup.slots_names):
+		# ~ setup.set_bias_voltage(slot_name, 0)
+		print(f'Bias voltage for slot {slot_name}: {setup.measure_bias_voltage(slot_name):.0f} V')
+		print(f'Bias current for slot {slot_name}: {setup.measure_bias_current(slot_name)*1e-6:.2f} µA')
+	print(f'Setup status is "{setup.status}"')
+	
+	print(f'Starting the setup...')
+	setup.start()
+	print(f'Setup has started!')
+	
 	print(f'Setup status is "{setup.status}"')
 	print(f'Climate chamber dryer: {setup.dryer}')
 	print(f'Climate chamber compressed air: {setup.compressed_air}')
@@ -209,7 +266,7 @@ if __name__ == '__main__':
 	print(f'Temperature: {setup.temperature:.2f} °C')
 	print(f'Humidity: {setup.humidity:.2f} %RH')
 	for idx, slot_name in enumerate(setup.slots_names):
-		setup.set_bias_voltage(slot_name, 0)
+		# ~ setup.set_bias_voltage(slot_name, 0)
 		print(f'Bias voltage for slot {slot_name}: {setup.measure_bias_voltage(slot_name):.0f} V')
 		print(f'Bias current for slot {slot_name}: {setup.measure_bias_current(slot_name)*1e-6:.2f} µA')
 	print(f'Setup status is "{setup.status}"')
